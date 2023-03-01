f3f4c4c9dd4919e19e73ad030d89bde46957cec2
nataliapovaliy <natalia.bg1125@gmail.com>
2023-03-01 18:54:15 +0200

    add getContactById

--- contacts.js
+++ contacts.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 const contactsPath = path.join(__dirname, './db/contacts.json');
 
-// TODO: задокументировать каждую функцию
 async function listContacts() {
     try {
         const data = await fs.readFile(contactsPath);
@@ -13,12 +12,19 @@ async function listContacts() {
         console.log(error);
     }
 }
-// listContacts();
 
-function getContactById(contactId) {
-    // try {
-        // const data = await fs.
-    // }
+async function getContactById(contactId) {
+    try {
+        const data = await fs.readFile(contactsPath);
+        const result = JSON.parse(data);
+        for (const key in result) {
+            if (result[key].id === contactId) {
+                console.table(result[key]);
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function removeContact(contactId) {
